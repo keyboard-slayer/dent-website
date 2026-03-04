bb63bd38f0becdce94330c59384f9b11d97edbf4
keyb <git@keyb.moe>
2026-03-04 19:20:14 +0100

    feat: can select and double click on prog

--- src/lib/components/Prog.svelte
+++ src/lib/components/Prog.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
-  let { icon, name } = $props();
+  let { icon, name, onClick, onDblClick } = $props();
 </script>
 
-<figure class="flex flex-col items-center w-18 cursor-default select-none mt-2">
-  <img src={icon} width="40" height="40" alt={name} class="mb-1" />
+<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<figure
+  class="flex flex-col items-center w-18 h-17 cursor-default select-none mt-2"
+  onclick={onClick}
+  ondblclick={onDblClick}
+>
+  <div class="relative mb-1">
+    <img src={icon} width="40" height="40" alt={name} />
+  </div>
   <figcaption class="text-xs text-center wrap-break-word leading-tight">
     {name}
   </figcaption>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,71 @@
 <script lang="ts">
   import Window from "$lib/components/Window.svelte";
   import Prog from "$lib/components/Prog.svelte";
+
+  let clicked: HTMLElement | null = $state(null);
+
+  function clickHandler(event: MouseEvent) {
+    function toggle(el: HTMLElement) {
+      let caption = el.getElementsByTagName("figcaption")[0].classList;
+      caption.toggle("text-white");
+      caption.toggle("bg-[#000080]");
+      caption.toggle("border-dotted");
+      caption.toggle("border-1");
+      caption.toggle("border-[#ffff00]");
+
+      let mask = document.getElementById("mask");
+      if (mask !== null) mask.remove();
+
+      let imgWrapper = el.querySelector(".relative") as HTMLElement;
+      let maskEl = document.createElement("div");
+      maskEl.classList.add(
+        "absolute",
+        "inset-0",
+        "bg-[image:repeating-conic-gradient(rgba(0,0,128,0.8)_0%_25%,transparent_0%_50%)]",
+        "bg-[size:2px_2px]",
+        "pointer-events-none",
+      );
+      maskEl.id = "mask";
+      maskEl.style.maskImage = `url(${el.getElementsByTagName("img")[0].src})`;
+      maskEl.style.maskSize = "40px 40px";
+      imgWrapper.append(maskEl);
+    }
+
+    event.stopPropagation();
+    const target = event.currentTarget as HTMLElement;
+
+    if (target === clicked) return;
+    if (clicked != null) toggle(clicked);
+    if (event.currentTarget === window) {
+      document.getElementById("mask")?.remove();
+      clicked = null;
+      return;
+    }
+
+    toggle(target);
+    clicked = target;
+  }
+
+  function dblClickHandler(event: MouseEvent) {
+    event.stopPropagation();
+    const target = event.currentTarget as HTMLElement;
+    alert(`You double-clicked on ${target.innerText}`);
+  }
 </script>
 
+<svelte:window on:click={clickHandler} />
+
 <Window title="Dent">
-  <Prog name="About Me" icon="/icons/w98_help_book_big.ico" />
-  <Prog name="My Artwork" icon="/icons/w98_frame_web.ico" />
+  <Prog
+    onClick={clickHandler}
+    onDblClick={dblClickHandler}
+    name="About Me"
+    icon="/icons/w98_help_book_big.ico"
+  />
+  <Prog
+    onClick={clickHandler}
+    onDblClick={dblClickHandler}
+    name="My Artwork"
+    icon="/icons/w98_frame_web.ico"
+  />
 </Window>
